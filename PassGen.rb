2a2e9ce34205b389803b3d6c80416562852ff7f9
Gregorio Pompei <pompeigregorio@gmail.com>
2018-09-14 16:29:14 +0200

    Add files via upload

---
+++ PassGen.rb
@@ -0,0 +1,17 @@
+= "italian"
+2 = "english"
+
+print "Select language [Ita][Eng]\n"
+lang = gets.chomp
+print "Insert your password length:\n"
+password = gets.chomp
+  o = [('a'..'z'), ('A'..'Z'), (0 .. 9)].map { |i| i.to_a }.flatten
+  string = (0...password.to_i).map { o[rand(o.length)] }.join
+
+  (0...8).map { (65 + rand(15)).chr }.join
+
+  (0...password.to_i).map {('a'..'z').to_a[rand(15)] }.join
+
+  print "This is your password : #{string}\n"  
+
+print #{lang}
---
+++ README.md
@@ -0,0 +1,10 @@
+# PassGen
+Here's my Password Generator
+
+## Git setup
+* Download the repository: 'https://github.com/gregoriopompei/PassGen.git'
+  
+### Local Setup
+* Run the rubyscript with the following command `ruby PassGen.rb`
+* Insert the length of the password and press enter.
+
